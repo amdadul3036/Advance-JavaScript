54ca5035acfadb49ff03f2ae1ee6c9ec66083b0e
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-07 09:06:50 -0700

    Wahgit add . I am now Intermediategit add .!

---
+++ undifinedVsNull.js
@@ -0,0 +1,10 @@
+When JavaScript will return undifined--
+  1. If do not give any value of a variable.
+  2. If do not return the function.
+  3. If do not pass the parameter when calling the function.
+  4. If do not have the property in a class but we want to get the property.
+  5. If we set the value of a variable "undifined".
+
+
+When to get Null--
+  1. 
