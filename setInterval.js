91d0e904fe7d6901107fe1fe6eb06012bbdae7d9
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-12 22:47:10 -0700

    setInterval

---
+++ setInterval.js
@@ -0,0 +1,5 @@
+console.log("I love you");
+console.log("100% Love");
+setInterval(() => console.log("HAHAHAHAH"),3000); // 3000 ms(mili second) e. 1 second = 1000 ms.
+console.log("Uh ah uh ah");
+ // That is "setInterval" jekhanei deya hok na keno se sob kaj sese kaj korbe and bar bar korbe . Ctrl+C press korle theme jabe.
--- setTimeOut.js
+++ setTimeOut.js
@@ -1,5 +1,5 @@
 console.log("I love you");
 console.log("100% Love");
-setTimeout(() => console.log("HAHAHAHAH"),3000);
+setTimeout(() => console.log("HAHAHAHAH"),3000); // 3000 ms(mili second) e. 1 second = 1000 ms.
 console.log("Uh ah uh ah");
  // That is "setTimeout" jekhanei deya hok na keno se sob kaj sese kaj korbe.
