d2701f1dce0e4d0e5a351595f84c6117b1e5a1b6
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-12 19:57:16 -0700

    SetTimeOut will work at the end of the programm; no matter where it is declared

--- setTimeOut.js
+++ setTimeOut.js
@@ -1,4 +1,5 @@
 console.log("I love you");
 console.log("100% Love");
-console.log("Uh ah uh ah");
 setTimeout(() => console.log("HAHAHAHAH"),3000);
+console.log("Uh ah uh ah");
+ // That is "setTimeout" jekhanei deya hok na keno se sob kaj sese kaj korbe.
